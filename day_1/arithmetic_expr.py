8f66f59d802a1f8273cf9a423c1cc49e56e16560
sergei <sokolov12ssa@gmail.com>
2026-06-18 14:23:30 +0300

    arithmetic expressions

---
+++ day_1/arithmetic_expr.py
@@ -0,0 +1,13 @@
+# Task 2. Get familiar with arithmetic expressions (integers)
+print(3 * 7, (17 - 2) * 8)
+print(2 ** 16)
+print(37 / 3)
+print(37 // 3)
+print(37 % 3)
+
+# Floats
+print(3.5 * 7, (17 - 2) * 8)
+print(2.5 ** 16)
+print(37.4 / 3)
+print(37.4 // 3)
+print(37.4 % 3)
---
+++ day_1/sum.py
@@ -0,0 +1,7 @@
+# Task 1. Calculate the sum of 2 numbers (float numbers)
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+y = a + b
+print(f"The sum of two numbers is: {y}")
+
+
